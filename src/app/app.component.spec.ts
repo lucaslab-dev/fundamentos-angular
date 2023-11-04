9a0dd95336100eac0e0e437cf2b0aeaf3b463109
Lucas <lucasluizss@live.com>
2023-11-04 00:54:34 +0000

    cleanup solution

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,10 +3,11 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -20,10 +21,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('fundamentos-angular');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fundamentos-angular app is running!');
-  });
+  //it('should render title', () => {
+  //  const fixture = TestBed.createComponent(AppComponent);
+  //  fixture.detectChanges();
+  //  const compiled = fixture.nativeElement as HTMLElement;
+  //  expect(compiled.querySelector('.content span')?.textContent).toContain(
+  //    'fundamentos-angular app is running!'
+  //  );
+  //});
 });
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideNoopAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(APP_ROUTES),
+  ],
+}).catch((err) => console.error(err));
