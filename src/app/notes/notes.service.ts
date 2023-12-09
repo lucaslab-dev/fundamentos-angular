e2d725f54a4044ddc82428b8ebea0c640c15b7f0
Lucas <lucasluizss@live.com>
2023-12-09 18:52:06 +0000

    creating service

---
+++ src/app/notes/notes.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotesService {
+  private notes = signal<string[]>([]);
+
+  list() {
+    return this.notes;
+  }
+
+  add(text: string) {
+    this.notes.update((items) => [...items, text]);
+  }
+}
